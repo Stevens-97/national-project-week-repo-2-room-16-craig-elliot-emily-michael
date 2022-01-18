b5752f7fb6094d71d17b18a67124bda5fdc4f349
Elliott.Stevens <Elliott.Samuel.Stevens@gmail.com>
2022-01-18 14:31:02 +0000

    Added router and added file structure

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,25 +1,37 @@
 import React from "react";
 import "./App.css";
-
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
-   const [data, setData] = React.useState(null);
 
-   React.useEffect(() => {
-      async function getMessage() {
-         console.log("useEffect in action");
-         const response = await fetch("http://localhost:5001/test");
-         const data = await response.json();
-         setData(data);
-         console.log(data.message);
-      }
-      getMessage();
-   }, []);
 
-   return (
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="feedback" element={<FeedbackSection />} />
+      </Routes>
       <div className="App">
-         <p>I'm working, check the console!</p>
+        <p>I'm working, check the console!</p>
       </div>
+    </div>
    );
 }
 
 export default App;
+
+
+// const [data, setData] = React.useState(null);
+
+// React.useEffect(() => {
+//    async function getMessage() {
+//       console.log("useEffect in action");
+//       const response = await fetch("http://localhost:5001/test");
+//       const data = await response.json();
+//       setData(data);
+//       console.log(data.message);
+//    }
+//    getMessage();
+// }, []);
