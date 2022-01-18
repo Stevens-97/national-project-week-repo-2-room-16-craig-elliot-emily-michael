dc7a48b8b366279df1a4b11eef72d0304f14f192
emilysarina <emy.sarina@gmail.com>
2022-01-18 12:01:11 +0000

    added fetch api

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,6 +1,15 @@
+import React from "react"
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
      
