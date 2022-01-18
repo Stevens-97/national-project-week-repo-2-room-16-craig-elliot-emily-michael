8a888324c7b313ec05ebc2b9a2de58167fc2823f
emilysarina <emy.sarina@gmail.com>
2022-01-18 16:27:26 +0000

    added input component

--- client/src/components/FeedbackSection/Dropdown/index.js
+++ client/src/components/FeedbackSection/Dropdown/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 export default function Dropdown({title , name}) {
     return (
         <div>
---
+++ client/src/components/FeedbackSection/DropdownInput/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function DropDownInput() {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
