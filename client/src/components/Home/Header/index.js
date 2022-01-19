33c6f9ad32e58bfaa12e8162a12610d4434116b8
emilysarina <emy.sarina@gmail.com>
2022-01-19 12:04:17 +0000

    started css

--- client/src/components/Home/Header/index.js
+++ client/src/components/Home/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import './style.css'
 
 export default function Header() {
     return (
-        <div>
+        <div className='header'>
         <Link to="/">
             <h1>
                 School of Code Bootcamper Portal
