9949ef70397fd2473fa172138e64dc5d185630d1
craigmaru <craigmarubeats@gmail.com>
2022-01-19 16:02:22 +0000

    backend link added to homepage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,19 +6,30 @@ import FeedbackIndex from "../FeedbackSection";
 import HomeIndex from "../Home";
 
 function App() {
+   // Use this for the backend url REACT_APP_API_URL = https://room16-project.herokuapp.com/
 
-  return (
-    <div>
-      <nav>
-      <Header/>
-      </nav>
-      <Routes>
-        <Route path="/" element={<HomeIndex />} />
-        <Route path="/feedback" element={<FeedbackIndex />} />
-      </Routes>
-      <div className="App">
+   // const REACT_APP_API_URL = "https://room16-project.herokuapp.com/"
+
+   React.useEffect(() => {
+      async function getUrl() {
+         const response = await fetch(process.env.REACT_APP_API_URL);
+         const data = await response.json();
+         console.log(data);
+      }
+      getUrl();
+   }, []);
+
+   return (
+      <div>
+         <nav>
+            <Header />
+         </nav>
+         <Routes>
+            <Route path="/" element={<HomeIndex />} />
+            <Route path="/feedback" element={<FeedbackIndex />} />
+         </Routes>
+         <div className="App"></div>
       </div>
-    </div>
    );
 }
 
@@ -27,7 +38,6 @@ function App() {
         <Route path="feedback" element={<FeedbackSection />} />
       </Routes>*/
 export default App;
-
 
 // const [data, setData] = React.useState(null);
 
