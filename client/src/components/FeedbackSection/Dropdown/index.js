ecec9077e27e8e5fbd5a3cdf655a9c1871de110c
Elliott.Stevens <Elliott.Samuel.Stevens@gmail.com>
2022-01-19 10:51:27 +0000

    Refactored data for drop down

--- client/src/components/FeedbackSection/Dropdown/index.js
+++ client/src/components/FeedbackSection/Dropdown/index.js
@@ -2,10 +2,17 @@ import React, {useState} from 'react'
 import DropdownInput from '../DropdownInput'
 
 const bootcamperData = [
-    {name: "Elliott"},
-    {name: "Emily"}
+    {value: "Elliott"},
+    {value: "Emily"},
+    {value: "Michael"},
+    {value: "Craig"}
 ]
-const bootcampData =[8,9,10,11]
+const bootcampData =[
+    {value: 8},
+    {value: 9},
+    {value: 10},
+    {value:11} 
+]
 
 export default function Dropdown({title , name}) {
     const [bootcampers, setBootcampers] = useState(bootcamperData)
