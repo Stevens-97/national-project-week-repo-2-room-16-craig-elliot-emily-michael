04d1686ca295dff5dc05ef95b35fafa46b9ec181
Your Name <you@example.com>
2022-01-20 10:09:52 +0000

    database linked to frontend

--- src/components/FeedbackSection/SubmitButton/index.js
+++ src/components/FeedbackSection/SubmitButton/index.js
@@ -2,21 +2,21 @@ import React, { useEffect } from 'react'
 
 export default function Submit({postData}) {
         // Sending off the data to database
-        // async function sendPostData(postData){
-        //     const res = await fetch("",{
-        //     method: 'POST',
-        //     body: JSON.stringify(postData),
-        //     headers: {
-        //     'Content-type': 'application/json; charset=UTF-8'
-        //     }})
-        //     const data = await res.json()
-        //     console.log(data)
-        // }
+        async function sendPostData(postData){
+            const res = await fetch("https://room16-project.herokuapp.com/users/",{
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+            }})
+            const data = await res.json()
+            console.log(data)
+        }
         
         function handleSubmit(e){
             e.preventDefault()
             console.log("handle submit working, here is the post data",postData )
-            // sendPostData()
+            sendPostData()
         }
     
     return (
