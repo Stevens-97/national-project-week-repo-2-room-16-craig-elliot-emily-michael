a4ab5de86598ed26b636aac6468c1a17b8947828
craigmaru <craigmarubeats@gmail.com>
2022-01-20 11:36:39 +0000

    front end names changed

--- src/components/FeedbackSection/SubmitButton/index.js
+++ src/components/FeedbackSection/SubmitButton/index.js
@@ -1,30 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-export default function Submit({postData}) {
-        // Sending off the data to database
-        async function sendPostData(postData){
-            console.log("POST DATA HERE", postData)
-            const res = await fetch("https://room16-project.herokuapp.com/users/",{
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-            }})
-            const data = await res.json()
-            console.log(data)
-        }
-        
-        function handleSubmit(e){
-            e.preventDefault()
-            console.log("handle submit working, here is the post data",postData )
-            sendPostData(postData)
-        }
-    
-    return (
-        <div>
-            <button type="submit" onClick={handleSubmit}>
-                Submit
-            </button>
-        </div>
-    )
+export default function Submit({ postData }) {
+   // Sending off the data to database
+   async function sendPostData(postData) {
+      console.log("POST DATA HERE", postData);
+      const res = await fetch("https://room16-project.herokuapp.com/users/", {
+         method: "POST",
+         body: JSON.stringify(postData),
+         headers: {
+            "Content-type": "application/json; charset=UTF-8",
+         },
+      });
+      const data = await res.json();
+      console.log(data);
+   }
+
+   function handleSubmit(e) {
+      e.preventDefault();
+      console.log("handle submit working, here is the post data", postData);
+      sendPostData(postData);
+   }
+
+   return (
+      <div>
+         <button type="submit" onClick={handleSubmit}>
+            Submit
+         </button>
+      </div>
+   );
 }
