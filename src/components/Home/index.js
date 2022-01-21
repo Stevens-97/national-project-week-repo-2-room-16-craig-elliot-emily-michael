62345a0ae1fed70fff10682a0235a30d4eea87ca
Elliott.Stevens <Elliott.Samuel.Stevens@gmail.com>
2022-01-21 09:17:22 +0000

    Changed div inline style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,12 +4,12 @@ import ReactHelp from './ReactHelp/index.js'
 
 export default function HomeIndex() {
     return (
-        <div>
+        <div style={{display:"flex",alignItems: "center", justifyContent: "center", flexDirection:"column"}}>
            <FeedbackHeader />
            <ReactHelp title='React' 
            url1='https://www.youtube.com/watch?v=O6P86uwfdR0' sub1={'UseState'} 
            url2='https://www.youtube.com/watch?v=0ZJgIjIuY7U' sub2={'UseEffect'} 
-           url3='https://www.youtube.com/watch?v=3e1GHCA3GP0' sub3={'React Testing'}/>
+           url3='https://www.youtube.com/watch?v=3e1GHCA3GP0' sub3={'React Testing'} />
            <ReactHelp title='Node.js' 
            url1={'https://www.youtube.com/watch?v=ENrzD9HAZK4'} sub1={'Basics'}
            url2={'https://www.youtube.com/watch?v=JlgKybraoy4'} sub2={'Express Basics'}
