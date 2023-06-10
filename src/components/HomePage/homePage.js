606160db7e8d9f1b22f4d21b6cf3a5e115567666
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-06-10 23:13:17 +0100

    Add to home page component and associated styling

--- src/components/HomePage/homePage.js
+++ src/components/HomePage/homePage.js
@@ -1,9 +1,39 @@
 import React from "react";
+import InfoBlock from "../InfoBlock/infoBlock";
+import {
+    firstMockInfoBlock,
+    secondMockInfoBlock,
+    thirdMockInfoBlock,
+} from "../../libs/infoBlockMockData";
+import CallToAction from "../CallToAction/callToAction";
+
+import style from "./homePage.module.css";
 
 export default function HomePage() {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  );
+    const { wrapperRightAligned, wrapperLeftAligned, wrapperCenterAligned } =
+        style;
+    return (
+        <div>
+            <CallToAction />
+            <div className={wrapperLeftAligned}>
+                <InfoBlock
+                    text={firstMockInfoBlock.text}
+                    imageUrl={firstMockInfoBlock.imageUrl}
+                    position="right"
+                />
+            </div>
+            <div className={wrapperRightAligned}>
+                <InfoBlock
+                    text={secondMockInfoBlock.text}
+                    imageUrl={secondMockInfoBlock.imageUrl}
+                />
+            </div>
+            <div className={wrapperLeftAligned}>
+                <InfoBlock
+                    text={thirdMockInfoBlock.text}
+                    imageUrl={thirdMockInfoBlock.imageUrl}
+                />
+            </div>
+        </div>
+    );
 }
