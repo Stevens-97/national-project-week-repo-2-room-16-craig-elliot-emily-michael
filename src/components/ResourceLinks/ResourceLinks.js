395801027bb677c2318e3c4d17ff7cdd720c4970
Elliot <elliott.samuel.stevens@gmail.com>
2023-08-07 19:05:08 +0100

    Make resource links mobile friendly

--- src/components/ResourceLinks/ResourceLinks.js
+++ src/components/ResourceLinks/ResourceLinks.js
@@ -2,29 +2,29 @@ import React from "react";
 import styles from "./resourceLinks.module.css";
 
 export default function ResourceLinks({ resources }) {
-  const { links, helpBox, heading } = styles;
+  const { links, helpBox, heading, resourceWrapper } = styles;
   const { tutorials, title } = resources;
 
   const resourceSection = resources
     ? tutorials.map((tutorial, index) => {
-        return (
-          <div className={links} key={`tutorial-${index}`}>
-            <a
-              href={tutorial?.videoUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {tutorial?.videoTitle}
-            </a>
-          </div>
-        );
-      })
+      return (
+        <div className={links} key={`tutorial-${index}`}>
+          <a
+            href={tutorial?.videoUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {tutorial?.videoTitle}
+          </a>
+        </div>
+      );
+    })
     : null;
 
   return (
     <div className={helpBox}>
       <h6 className={heading}>{title}</h6>
-      <div>{resourceSection}</div>
+      <div className={resourceWrapper}>{resourceSection}</div>
     </div>
   );
 }
