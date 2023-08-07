499fdba0e546d0b7ae8bf345c7ac8ebfd6e93a4a
Elliot <elliott.samuel.stevens@gmail.com>
2023-08-07 17:18:18 +0100

    Change mockInfoBlock data layout

--- src/libs/infoBlockMockData.js
+++ src/libs/infoBlockMockData.js
@@ -1,22 +1,22 @@
-const firstMockInfoBlock = {
-    text: [
-        "Submitting a daily feedback form offers multiple benefits to bootcampers. It provides a concise summary of their work completed, allowing them to track progress and achievements. Rating how they felt about their work offers valuable insights into their motivation and satisfaction.",
-    ],
-    imageUrl: "/static/note_on_board.jpg",
-};
-
-const secondMockInfoBlock = {
-    text: [
-        "Accessing the collated user feedback data on the dashboard section of the site brings valuable benefits. It provides an overview of collective experiences and sentiments, revealing patterns and trends. This data-driven approach enables identifying areas for improvement and successful strategies.",
-    ],
-    imageUrl: "/static/graph_on_table.jpg",
-};
-
-const thirdMockInfoBlock = {
+const mockInfoBlock = [{
+    title: "Access useful resources to improve learning",
     text: [
         "The website's resource section offers users a valuable tool to study and improve their skills. It provides a curated collection of useful links that encompass a wide range of topics relevant to bootcampers.",
     ],
     imageUrl: "/static/books_on_table.jpg",
-};
+}, {
+    title: "Analyse trends in user data",
+    text: [
+        "Accessing the collated user feedback data on the dashboard section of the site brings valuable benefits. It provides an overview of collective experiences and sentiments, revealing patterns and trends. This data-driven approach enables identifying areas for improvement and successful strategies.",
+    ],
+    imageUrl: "/static/graph_on_table.jpg",
+}
+    , {
+    title: "Feedback aids reflection",
+    text: [
+        "Submitting a daily feedback form offers multiple benefits to bootcampers. It provides a concise summary of their work completed, allowing them to track progress and achievements. Rating how they felt about their work offers valuable insights into their motivation and satisfaction.",
+    ],
+    imageUrl: "/static/note_on_board.jpg",
+}]
 
-export { firstMockInfoBlock, secondMockInfoBlock, thirdMockInfoBlock };
+export { mockInfoBlock };
