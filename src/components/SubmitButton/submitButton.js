28f04a49b1365b9af2b57f26db319ebfcb6c7811
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-09-03 13:20:34 +0100

    Update submitButton component logic and styling

--- src/components/SubmitButton/submitButton.js
+++ src/components/SubmitButton/submitButton.js
@@ -1,31 +1,26 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./submitButton.module.css";
+export default function Submit({ postData, link, disabled = false }) {
+    const { submitButton, submitButtonContainer } = styles;
 
-export default function Submit({ postData, link }) {
-  // Sending off the data to database
-  async function sendPostData(postData) {
-    // const res = await fetch(`${REACT_APP_herokuURLPath}/users/`, {
-    //   method: "POST",
-    //   body: JSON.stringify(postData),
-    //   headers: {
-    //     "Content-type": "application/json; charset=UTF-8",
-    //   },
-    // });
-    // const data = await res.json();
-  }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("Debug here is the data: ", postData);
+    }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    sendPostData(postData);
-  }
-
-  return (
-    <div>
-      <button className="submit-buttons" type="submit" onClick={handleSubmit}>
-        <Link to={link} className="feedbacklink">
-          Submit
-        </Link>
-      </button>
-    </div>
-  );
+    return (
+        <div className={submitButtonContainer}>
+            <button
+                className={submitButton}
+                type="submit"
+                onClick={handleSubmit}
+                disabled={disabled}
+            >
+                <Link to={link} className="feedbacklink">
+                    Submit
+                </Link>
+            </button>
+        </div>
+    );
 }
