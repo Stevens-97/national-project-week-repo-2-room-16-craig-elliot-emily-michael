03a3f7793791fbabaa4d968d2783340d6c0a446d
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-09-03 13:17:21 +0100

    Update feedbackSection component logic and styling

--- src/components/FeedbackSection/feedbackSection.js
+++ src/components/FeedbackSection/feedbackSection.js
@@ -1,89 +1,121 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import Title from "../Title";
-//import Dropdown from './Dropdown/index.js'
 import TimeInput from "../TimeInput";
 import Feedback from "../Feedback/";
 import Rating from "../Rating";
-import {
-  bootcampData,
-  bootcamperData,
-  emptyFeedback,
-} from "../../libs/data.js";
 import BasicDropdown from "../BasicDropdown";
 import Submit from "../SubmitButton";
-
+import { data } from "../../libs/questionData";
 import styles from "./FeedbackSection.module.css";
 
 export default function FeedbackIndex() {
-  const { feedbackSection, input } = styles;
+    const {
+        feedbackSection,
+        button,
+        buttonContainer,
+        questionBox,
+        navigationButtons,
+    } = styles;
 
-  const [bootcampers, setBootcampers] = useState(bootcamperData);
-  const [bootcamps, setBootcamps] = useState(bootcampData);
-  // Form collection
-  const [feedbackInput, setFeedbackInput] = useState("");
-  const [bootcamper, setBootcamper] = useState("");
-  const [bootcamp, setBootcamp] = useState("");
-  const [todaysDate, setTodaysDate] = useState("");
-  const [workShopLectures, setWorkShopLectures] = useState("");
-  const [guestRating, setGuestRating] = useState("");
-  const [moodRating, setMoodRating] = useState("");
-  const [BoTW, setBoTW] = useState("");
-  // Form Data pooling
-  const [postData, setPostData] = useState(emptyFeedback);
+    const [answer, setAnswer] = useState({});
 
-  // useEffect(() => {
-  //   function updatePostData() {
-  //     setPostData({
-  //       ...postData,
-  //       name: bootcamper,
-  //       chort: bootcamp,
-  //       date: todaysDate,
-  //       dailyFeedBack: feedbackInput,
-  //       workShopRating: workShopLectures,
-  //       guestLectureRating: guestRating,
-  //       userFeelingRating: moodRating,
-  //       bootcamperOfTheWeek: BoTW,
-  //     });
-  //   }
-  //   updatePostData();
-  // }, [
-  //   feedbackInput,
-  //   bootcamper,
-  //   bootcamp,
-  //   todaysDate,
-  //   workShopLectures,
-  //   guestRating,
-  //   moodRating,
-  //   BoTW,
-  // ]);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [answerRequired, setAnswerRequired] = useState(false);
 
-  return (
-    <div className={feedbackSection}>
-      <Title text={"Feedback For The Day"} />
-      <BasicDropdown
-        setValue={setBootcamper}
-        title={"Name"}
-        data={bootcampers}
-        className={input}
-      />
-      <BasicDropdown
-        setValue={setBootcamp}
-        title={"Cohort"}
-        data={bootcamps}
-        className={input}
-      />
-      <TimeInput setValue={setTodaysDate} className="first-inputs" />
-      <Feedback setValue={setFeedbackInput} text={"Daily Feedback"} />
-      <Rating setValue={setWorkShopLectures} title={"Workshops / Lectures"} />
-      <Rating setValue={setGuestRating} title={"Guest Lectures"} />
-      <Rating setValue={setMoodRating} title={"Mood"} />
-      <BasicDropdown
-        setValue={setBoTW}
-        title={"Bootcamper of The Week"}
-        data={bootcampers}
-      />
-      <Submit link={"/anonymous-feed-back"} postData={postData} />
-    </div>
-  );
+    const handlePreviousClick = () => {
+        setCurrentQuestionIndex(Math.max(currentQuestionIndex - 1, 0));
+    };
+
+    const handleNextClick = () => {
+        setCurrentQuestionIndex(
+            Math.min(currentQuestionIndex + 1, data.length - 1)
+        );
+    };
+
+    let currentQuestion;
+
+    switch (data[currentQuestionIndex].type) {
+        case "dropdown":
+            currentQuestion = (
+                <BasicDropdown
+                    setValue={setAnswer}
+                    title={data[currentQuestionIndex]?.title}
+                    data={data[currentQuestionIndex]?.dropdown}
+                    index={data[currentQuestionIndex]?.index}
+                    value={answer}
+                />
+            );
+            break;
+        case "timeInput":
+            currentQuestion = (
+                <TimeInput
+                    setValue={setAnswer}
+                    value={answer}
+                    title={data[currentQuestionIndex].title}
+                    index={data[currentQuestionIndex]?.index}
+                    setAnswerRequired={setAnswerRequired}
+                />
+            );
+            break;
+        case "input":
+            currentQuestion = (
+                <Feedback
+                    setValue={setAnswer}
+                    value={answer}
+                    index={data[currentQuestionIndex]?.index}
+                    title={data[currentQuestionIndex].title}
+                />
+            );
+            break;
+        case "rating":
+            currentQuestion = (
+                <Rating
+                    setValue={setAnswer}
+                    value={answer}
+                    index={data[currentQuestionIndex]?.index}
+                    title={data[currentQuestionIndex].title}
+                />
+            );
+            break;
+        default:
+            currentQuestion = null;
+            break;
+    }
+
+    const isNextButtonDisabled =
+        currentQuestionIndex === data.length - 1 ||
+        (answerRequired && data?.[currentQuestionIndex].type === "timeInput");
+
+    return (
+        <div className={feedbackSection}>
+            <div className={questionBox}>
+                <h3>{currentQuestion}</h3>
+            </div>
+            <div className={buttonContainer}>
+                <div className={navigationButtons}>
+                    <button
+                        onClick={handlePreviousClick}
+                        disabled={currentQuestionIndex === 0}
+                        className={button}
+                    >
+                        Previous
+                    </button>
+                    {currentQuestionIndex === data.length - 1 ? (
+                        <Submit
+                            link={"/anonymous-feed-back"}
+                            postData={answer}
+                        />
+                    ) : (
+                        <button
+                            onClick={handleNextClick}
+                            disabled={isNextButtonDisabled}
+                            className={button}
+                        >
+                            Next
+                        </button>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
