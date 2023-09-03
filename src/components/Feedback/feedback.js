a8297f5722193eb5fb399b423a4d0f0059ffbf5e
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-09-03 13:17:10 +0100

    Update feedback component logic and styling

--- src/components/Feedback/feedback.js
+++ src/components/Feedback/feedback.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styling from "./feedback.module.css";
 
-export default function Feedback({ text, setValue }) {
-  const { input } = styling
+export default function Feedback({ title, setValue, index, value }) {
+    const { input } = styling;
 
-  function handleEvent(e) {
-    setValue(e.target.value);
-  }
-  return (
-    <div>
-      <label>{text}</label>
-      <br></br>
-      <textarea onChange={handleEvent} className={input}></textarea>
-    </div>
-  );
+    function handleEvent(e) {
+        setValue((prevState) => {
+            const updatedState = { ...prevState };
+            updatedState[`answer-${index}`] = e.target.value;
+            return updatedState;
+        });
+    }
+
+    useEffect(() => {
+        if (!value?.[[`answer-${index}`]]) {
+            setValue((prevState) => {
+                const updatedState = { ...prevState };
+                updatedState[`answer-${index}`] = "";
+                return updatedState;
+            });
+        }
+    }, [index]);
+
+    return (
+        <div>
+            <label>{title}</label>
+            <br></br>
+            <textarea onChange={handleEvent} className={input}></textarea>
+        </div>
+    );
 }
