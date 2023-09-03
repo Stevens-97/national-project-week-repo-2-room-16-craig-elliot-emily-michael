cb9eaeb19c6d1a4e9e164c55f71e6bdf4d92aaea
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-09-03 13:13:58 +0100

    Update timeInput component logic and styling

--- src/components/TimeInput/timeInput.js
+++ src/components/TimeInput/timeInput.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./timeInput.module.css";
 
-export default function TimeInput({ setValue }) {
-  function handleEvent(e) {
-    setValue(e.target.value);
-  }
-  return (
-    <div>
-      <label>Date</label>
-      <br></br>
-      <input onChange={handleEvent} className="input-box" type="date"></input>
-    </div>
-  );
+export default function TimeInput({
+    setValue,
+    title,
+    index,
+    value = null,
+    setAnswerRequired,
+}) {
+    const { inputBox } = styles;
+    const [localValue, setLocalValue] = useState(
+        value ? value[`answer-${index}`] : ""
+    );
+
+    useEffect(() => {
+        if (!localValue) {
+            setAnswerRequired(true);
+        } else {
+            setAnswerRequired(false);
+            setValue((prevState) => {
+                const updatedState = { ...prevState };
+                updatedState[`answer-${index}`] = localValue;
+                return updatedState;
+            });
+        }
+    }, [index, localValue, setAnswerRequired, setValue]);
+
+    function handleEvent(e) {
+        setLocalValue(e.target.value);
+    }
+
+    return (
+        <div>
+            <label>{title}</label>
+            <br></br>
+            <input
+                onChange={handleEvent}
+                className={inputBox}
+                type="date"
+                value={localValue}
+            />
+        </div>
+    );
 }
