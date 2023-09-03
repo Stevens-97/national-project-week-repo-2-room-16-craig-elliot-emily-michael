1c5efdc4985d380f47e5840c8618535e30ac99e3
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-09-03 13:11:54 +0100

    Update resource mock data

--- src/libs/resourceMockData.js
+++ src/libs/resourceMockData.js
@@ -1,72 +1,107 @@
 const resourceMockData = [
-  {
-    title: "React",
-    tutorials: [
-      {
-        videoUrl: "https://www.youtube.com/watch?v=O6P86uwfdR0",
-        videoTitle: "UseState",
-      },
-      {
-        videoUrl: "https://www.youtube.com/watch?v=0ZJgIjIuY7U",
-        videoTitle: "UseEffect",
-      },
-      {
-        videoUrl: "https://www.youtube.com/watch?v=3e1GHCA3GP0",
-        videoTitle: "React Testing",
-      },
-    ],
-  },
-  {
-    title: "Node.js",
-    tutorials: [
-      {
-        videoUrl: "https://www.youtube.com/watch?v=ENrzD9HAZK4",
-        videoTitle: "Basics",
-      },
-      {
-        videoUrl: "https://www.youtube.com/watch?v=JlgKybraoy4",
-        videoTitle: "Express Basics",
-      },
-      {
-        videoUrl: "https://www.youtube.com/watch?v=kV6MJ9W4whM",
-        videoTitle: "Nodemon",
-      },
-    ],
-  },
-  {
-    title: "Git",
-    tutorials: [
-      {
-        videoUrl: "https://www.youtube.com/watch?v=USjZcfj8yxE",
-        videoTitle: "Basics",
-      },
-      {
-        videoUrl: "https://www.youtube.com/watch?v=JTE2Fn_sCZs",
-        videoTitle: "Branching",
-      },
-      {
-        videoUrl: "https://ww.youtube.com/watch?v=0iuqXh0oojo",
-        videoTitle: "Merging",
-      },
-    ],
-  },
-  {
-    title: "Stressed",
-    tutorials: [
-      {
-        videoUrl: "https://www.afiniti.com/corporate/rock-paper-scissors",
-        videoTitle: "First surprise",
-      },
-      {
-        videoUrl: "https://www.youtube.com/watch?v=SB-qEYVdvXA",
-        videoTitle: "Second surprise",
-      },
-      {
-        videoUrl: "https://www.youtube.com/watch?v=xWRHTpqQMGM",
-        videoTitle: "Third surprise",
-      },
-    ],
-  },
+    {
+        title: "React",
+        tutorials: [
+            {
+                videoUrl: "https://www.youtube.com/watch?v=O6P86uwfdR0",
+                videoTitle: "UseState",
+            },
+            {
+                videoUrl: "https://www.youtube.com/watch?v=0ZJgIjIuY7U",
+                videoTitle: "UseEffect",
+            },
+            {
+                videoUrl: "https://www.youtube.com/watch?v=3e1GHCA3GP0",
+                videoTitle: "React Testing",
+            },
+            {
+                videoUrl:
+                    "https://www.youtube.com/watch?v=TNhaISOUy6Q&ab_channel=Fireship",
+                videoTitle: "React hooks",
+            },
+            {
+                videoUrl: "https://youtu.be/6ThXsUwLWvc?si=UGKDHMRFP0MIzANk",
+                videoTitle: "Custom hooks",
+            },
+            {
+                videoUrl:
+                    "https://www.youtube.com/watch?v=ZawAwPYrxGA&ab_channel=TelmoSampaio",
+                videoTitle: "CSS modules",
+            },
+        ],
+    },
+    {
+        title: "Node.js",
+        tutorials: [
+            {
+                videoUrl: "https://www.youtube.com/watch?v=ENrzD9HAZK4",
+                videoTitle: "Basics",
+            },
+            {
+                videoUrl: "https://www.youtube.com/watch?v=JlgKybraoy4",
+                videoTitle: "Express Basics",
+            },
+            {
+                videoUrl: "https://www.youtube.com/watch?v=kV6MJ9W4whM",
+                videoTitle: "Nodemon",
+            },
+            {
+                videoUrl:
+                    "https://www.youtube.com/watch?v=-MTSQjw5DrM&pp=ygULbm9kZSBqcyBhcGk%3D",
+                videoTitle: "RESTful APIs",
+            },
+            {
+                videoUrl:
+                    "https://www.youtube.com/watch?v=i9hOCvBDMMg&ab_channel=Steve%28Builder.io%29",
+                videoTitle: "Debugging tip",
+            },
+        ],
+    },
+    {
+        title: "Git",
+        tutorials: [
+            {
+                videoUrl: "https://www.youtube.com/watch?v=USjZcfj8yxE",
+                videoTitle: "Basics",
+            },
+            {
+                videoUrl: "https://www.youtube.com/watch?v=JTE2Fn_sCZs",
+                videoTitle: "Branching",
+            },
+            {
+                videoUrl: "https://ww.youtube.com/watch?v=0iuqXh0oojo",
+                videoTitle: "Merging",
+            },
+            {
+                videoUrl:
+                    "https://www.youtube.com/watch?v=f1wnYdLEpgI&pp=ygUMZ2l0IHJlYmFzaW5n",
+                videoTitle: "Rebasing",
+            },
+            {
+                videoUrl:
+                    "https://www.youtube.com/watch?v=i657Bg_HAWI&pp=ygUPZ2l0IGNoZXJyeSBwaWNr",
+                videoTitle: "Cherry picking",
+            },
+        ],
+    },
+    {
+        title: "Stressed",
+        tutorials: [
+            {
+                videoUrl:
+                    "https://www.afiniti.com/corporate/rock-paper-scissors",
+                videoTitle: "First surprise",
+            },
+            {
+                videoUrl: "https://www.youtube.com/watch?v=SB-qEYVdvXA",
+                videoTitle: "Second surprise",
+            },
+            {
+                videoUrl: "https://www.youtube.com/watch?v=xWRHTpqQMGM",
+                videoTitle: "Third surprise",
+            },
+        ],
+    },
 ];
 
 export default resourceMockData;
