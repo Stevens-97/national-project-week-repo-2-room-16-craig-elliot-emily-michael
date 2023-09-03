d19e2df175e6be3d1a0ae7d1323ece93d26476d7
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-09-03 13:19:31 +0100

    Update resourceLinks component logic and styling

--- src/components/ResourceLinks/ResourceLinks.js
+++ src/components/ResourceLinks/ResourceLinks.js
@@ -1,30 +1,45 @@
-import React from "react";
-import styles from "./resourceLinks.module.css";
+import React, { useRef } from "react";
+import styles from "./ResourceLinks.module.css";
+import classNames from "classnames";
 
 export default function ResourceLinks({ resources }) {
-  const { links, helpBox, heading, resourceWrapper } = styles;
-  const { tutorials, title } = resources;
+    const {
+        resourceContainer,
+        link,
+        helpBox,
+        heading,
+        resourceWrapper,
+        linkContainer,
+    } = styles;
+    const { tutorials, title, imageLink } = resources;
+    const carouselRef = useRef(null);
 
-  const resourceSection = resources
-    ? tutorials.map((tutorial, index) => {
-      return (
-        <div className={links} key={`tutorial-${index}`}>
-          <a
-            href={tutorial?.videoUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {tutorial?.videoTitle}
-          </a>
+    const resourceSection = resources
+        ? tutorials.map((tutorial, index) => {
+              return (
+                  <div className={resourceContainer} key={`tutorial-${index}`}>
+                      {imageLink ? (
+                          <image src={imageLink} height="100%" width="100%" />
+                      ) : null}
+                      <div className={linkContainer}>
+                          <a
+                              className={link}
+                              href={tutorial?.videoUrl}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                          >
+                              {tutorial?.videoTitle}
+                          </a>
+                      </div>
+                  </div>
+              );
+          })
+        : null;
+
+    return (
+        <div className={helpBox} ref={carouselRef}>
+            <h6 className={heading}>{title}</h6>
+            <div className={resourceWrapper}>{resourceSection}</div>
         </div>
-      );
-    })
-    : null;
-
-  return (
-    <div className={helpBox}>
-      <h6 className={heading}>{title}</h6>
-      <div className={resourceWrapper}>{resourceSection}</div>
-    </div>
-  );
+    );
 }
