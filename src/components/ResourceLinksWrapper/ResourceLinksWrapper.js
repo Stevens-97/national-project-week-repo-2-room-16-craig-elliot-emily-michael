b1170169a7da0b4880652b9f4ef44a6034623748
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-09-03 13:19:44 +0100

    Update resourceLinksWrapper component logic and styling

--- src/components/ResourceLinksWrapper/ResourceLinksWrapper.js
+++ src/components/ResourceLinksWrapper/ResourceLinksWrapper.js
@@ -5,13 +5,16 @@ import resourceMockData from "../../libs/resourceMockData";
 import styles from "./resourceLinksWrapper.module.css";
 
 export default function ResourceLinksWrapper() {
-  const { wrapper } = styles;
+    const { wrapper } = styles;
 
-  const resourceSection = resourceMockData.map((resources, index) => {
-    return (
-      <ResourceLinks resources={resources} key={`resource-link-${index}`} />
-    );
-  });
+    const resourceSection = resourceMockData.map((resources, index) => {
+        return (
+            <ResourceLinks
+                resources={resources}
+                key={`resource-link-${index}`}
+            />
+        );
+    });
 
-  return <div className={wrapper}>{resourceSection}</div>;
+    return <div className={wrapper}>{resourceSection}</div>;
 }
